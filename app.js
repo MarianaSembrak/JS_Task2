7c3d444f49dbcded6798b589045f0f1502a777c3
MarianaSembrak <117520714+MarianaSembrak@users.noreply.github.com>
2022-12-08 11:09:11 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,104 @@
+//Task2
+
+const months = prompt("Please, enter month of year");
+switch (months) {
+  case "1":
+    alert("Winter");
+    break;
+  case "2":
+    alert("Winter");
+    break;
+  case "3":
+    alert("Spring");
+    break;
+  case "4":
+    alert("Spring");
+    break;
+  case "5":
+    alert("Spring");
+    break;
+  case "6":
+    alert("Summer");
+    break;
+  case "7":
+    alert("Summer");
+    break;
+  case "8":
+    alert("Summer");
+    break;
+  case "9":
+    alert("Autumn");
+    break;
+  case "10":
+    alert("Autumn");
+    break;
+  case "11":
+    alert("Autumn");
+    break;
+  case "12":
+    alert("Winter");
+    break;
+
+  default:
+    alert("You enter incorrect data!");
+    break;
+}
+
+//Task1
+
+let count = 0;
+let q = prompt("Скільки буде 7*2?");
+console.log(q);
+if (q == 14) {
+  count = count + 1;
+}
+q = prompt("Скільки буде 7*3?");
+console.log(q);
+if (q == 21) {
+  count = count + 1;
+}
+q = prompt("Скільки буде 7*4?");
+console.log(q);
+if (q == 28) {
+  count = count + 1;
+}
+q = prompt("Скільки буде 7*5?");
+console.log(q);
+if (q == 35) {
+  count = count + 1;
+}
+q = prompt("Скільки буде 7*6?");
+console.log(q);
+if (q == 42) {
+  count = count + 1;
+}
+q = prompt("Скільки буде 7*7?");
+console.log(q);
+if (q == 49) {
+  count = count + 1;
+}
+q = prompt("Скільки буде 7*8?");
+console.log(q);
+if (q == 56) {
+  count = count + 1;
+}
+q = prompt("Скільки буде 7*9?");
+console.log(q);
+if (q == 63) {
+  count = count + 1;
+}
+q = prompt("Скільки буде 7*10?");
+console.log(q);
+if (q == 70) {
+  count = count + 1;
+}
+if (count <= 4) {
+  console.log("Ви відповіли на", count, "запитань з 9 можливих");
+  alert("Вчіться краще");
+} else if ((count > 4, count <= 8)) {
+  console.log("Ви відповіли на", count, "запитань з 9 можливих");
+  alert("Молодець");
+} else if (count == 9) {
+  console.log("Ви відповіли на", count, "запитань з 9 можливих");
+  alert("Ідеально");
+}
